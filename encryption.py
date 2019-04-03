9b5dba1137099d9f1ebafbf9b0df98296e922f99
philprocida37 <42648500+philprocida37@users.noreply.github.com>
2019-04-03 18:49:57 -0400

    Fixing read statements to be more efficient.

--- encryption.py
+++ encryption.py
@@ -1,132 +1,124 @@
 import random, os
 
 try:
-		testerKeys = open("keys.txt", "r")
-		try:
-				testerRels = open("rels.txt", "r")
-				try:
-						testerMessage = open("message.txt", "r")
-						testerKeys.close()
-						testerRels.close()
-						testerMessage.close()
+        testerKeys = open("keys.txt", "r")
+        try:
+                testerRels = open("rels.txt", "r")
+                try:
+                        testerMessage = open("message.txt", "r")
+                        testerKeys.close()
+                        testerRels.close()
+                        testerMessage.close()
 
-						messageCoords = []
-						randomizedMessage = []
-						decodeCoords = []
-						mess = []
-						# read from rels.txt
-						switchBoard = eval(open("rels.txt").read())
+                        messageCoords = []
+                        randomizedMessage = []
+                        decodeCoords = []
+                        mess = []
+                        # read from rels.txt
+                        switchBoard = eval(open("rels.txt").read())
 
-						# read from keys.txt
-						charValue = eval(open("keys.txt").read())
+                        # read from keys.txt
+                        charValue = eval(open("keys.txt").read())
 
-						def convertMessage(message):
-								global mess
-								message = list(message)
-								iters = 0
-								while iters < len(message):
-										n = charValue.get(message[iters], "")
-										mess.append(n)
-										iters += 1
-								return ''.join(mess)
+                        def convertMessage(message):
+                                global mess
+                                message = list(message)
+                                iters = 0
+                                while iters < len(message):
+                                        n = charValue.get(message[iters], "")
+                                        mess.append(n)
+                                        iters += 1
+                                return ''.join(mess)
 
-						def deconvertMessage(message):
-							size = len(next(iter(charValue.values())))
-							message = [message[i:i+size] for i in range(0, len(message), size)]
-							iters = 0
-							deConverted = []
-							while iters < len(message):
-								try:
-										s = list(charValue.keys())[list(charValue.values()).index(message[iters])]
-										deConverted.append(str(s))
-										iters += 1
-								except:
-										print("Oops, wrong key.")
-										break
-							return ''.join(deConverted)
+                        def deconvertMessage(message):
+                            size = len(next(iter(charValue.values())))
+                            message = [message[i:i+size] for i in range(0, len(message), size)]
+                            iters = 0
+                            deConverted = []
+                            while iters < len(message):
+                                try:
+                                        s = list(charValue.keys())[list(charValue.values()).index(message[iters])]
+                                        deConverted.append(str(s))
+                                        iters += 1
+                                except:
+                                        print("Oops, wrong key.")
+                                        break
+                            return ''.join(deConverted)
 
 
-						def switch(message):
-								if len(str(message)) > 1:
-										print("Value too large.")
-										return None
-								return switchBoard.get(message, "")
+                        def switch(message):
+                                if len(str(message)) > 1:
+                                        print("Value too large.")
+                                        return None
+                                return switchBoard.get(message, "")
 
-						def reverseLookUp(message):
-								try:
-										return list(switchBoard.keys())[list(switchBoard.values()).index(message)]
-								except:
-										return print("Oops, wrong key.")
+                        def reverseLookUp(message):
+                                try:
+                                        return list(switchBoard.keys())[list(switchBoard.values()).index(message)]
+                                except:
+                                        return print("Oops, wrong key.")
 
-						def encodeLayerTwo(message):
-								# write to message.txt
-								iters = 0
-								global randomizedMessage
-								global decodeCoords
-								global messageCoords
-								message = list(message)
-								while iters < len(message):
-										try:
-												uIn = message[iters]
-										except:
-												print("Error")
-												break
+                        def encodeLayerTwo(message):
+                                # write to message.txt
+                                iters = 0
+                                global randomizedMessage
+                                global decodeCoords
+                                global messageCoords
+                                message = list(message)
+                                while iters < len(message):
+                                    try:
+                                        uIn = message[iters]
+                                    except:
+                                        print("Error")
+                                        break
+                                    y = random.randint(1, 100)
+                                    i = 0
+                                    try:
+                                        x = switch(uIn)
+                                    except:
+                                        break
+                                    while i < y:
+                                        x = switch(x)
+                                        i += 1
+                                    messageCoords.append([i, x]) # write this to message.txt, each coord has a line
+                                    coordsIter = 0
+                                    exporter = open("message.txt", "w")
+                                    while(coordsIter < len(messageCoords)):
+                                        exporter.write(str(messageCoords[coordsIter][0])+" "+str(messageCoords[coordsIter][1]) + "\n")
+                                        coordsIter += 1
+                                    exporter.close()
+                                    iters += 1
+                                print("Message generated.")
 
-										y = random.randint(1, 100)
-										i = 0
-										try:
-												x = switch(uIn)
-										except:
-												break
-										while i < y:
-												x = switch(x)
-												i += 1
-										messageCoords.append([i, x]) # write this to message.txt, each coord has a line
-										coordsIter = 0
-										if(os.stat("message.txt").st_size != 0):
-												exporter = open("message.txt", "w")
-												while(coordsIter < len(messageCoords)):
-													  exporter.write(str(messageCoords[coordsIter][0])+" "+str(messageCoords[coordsIter][1]) + "\n")
-													  coordsIter += 1
-												exporter.close()
-										else:
-												exporter = open("message.txt", "a")
-												while(coordsIter < len(messageCoords)):
-													  exporter.write(str(messageCoords[coordsIter])+"\n")
-													  coordsIter += 1
-												exporter.close()
-										iters += 1
-								print("Message generated.")
-
-						def decodeLayerTwo():
-								# read from message.txt ; remove input requirement for function
-								# scan each line from message.txt, placing it into the gatheredMess array at the appropriate index
-								gatherMess = []
-								with open("message.txt", "r") as importer:
-										for line in importer:
-												splitLineItems = line.split()
-												gatherMess.append([int(splitLineItems[0]), splitLineItems[1]])
-								returnMessage = []
-								numRev = 0
-								mainArr = 0
-								while mainArr < len(gatherMess):
-										numRev = gatherMess[mainArr][1]
-										iters = 0
-										while iters < gatherMess[mainArr][0]+1:
-												prev = numRev
-												numRev = reverseLookUp(str(numRev))
-												iters += 1
-										returnMessage.append(str(numRev))
-										mainArr += 1
-								return ''.join(returnMessage)
-						# Generate keys and rels, input message --> Encryption
-						# Use keys, rels, and coords --> Decryption
-								# Encryption: convertMessage --> encodeLayerTwo
-								# Decryption: decodeLayerTwo --> deconvertMessage               
-				except:
-						print("message file not found")
-		except:
-				print("rels file not found")
+                        def decodeLayerTwo():
+                                # read from message.txt ; remove input requirement for function
+                                # scan each line from message.txt, placing it into the gatheredMess array at the appropriate index
+                                gatherMess = []
+                                with open("message.txt", "r") as importer:
+                                        for line in importer:
+                                                splitLineItems = line.split()
+                                                gatherMess.append([int(splitLineItems[0]), splitLineItems[1]])
+                                returnMessage = []
+                                numRev = 0
+                                mainArr = 0
+                                while mainArr < len(gatherMess):
+                                        numRev = gatherMess[mainArr][1]
+                                        iters = 0
+                                        while iters < gatherMess[mainArr][0]+1:
+                                                prev = numRev
+                                                numRev = reverseLookUp(str(numRev))
+                                                iters += 1
+                                        returnMessage.append(str(numRev))
+                                        mainArr += 1
+                                return ''.join(returnMessage)
+                        # Generate keys and rels, input message --> Encryption
+                        # Use keys, rels, and coords --> Decryption
+                                # Encryption: convertMessage --> encodeLayerTwo
+                                # Decryption: decodeLayerTwo --> deconvertMessage               
+                except:
+                        print("message file not found")
+        except:
+                print("rels file not found")
 except:
-		print("keys file not found")
+        print("keys file not found")
 
--- keyGenerator.py
+++ keyGenerator.py
@@ -24,22 +24,14 @@ def charRelsGen():
 
     printRel = 0
     #print("switchBoard = {")
-    if(os.stat("rels.txt").st_size != 0):
-        exporter = open("rels.txt", "w")
-        exporter.write("{")
-        while(printRel < len(switchBoard)):
-            exporter.write("\t\""+str(switchBoard[printRel][0]) + "\""+ ":\"" + str(switchBoard[printRel][1]) + "\", ")
-            printRel += 1
-        exporter.write("}")
-        exporter.close()
-    else:
-        exporter = open("rels.txt", "a")
-        exporter.write("{")
-        while(printRel < len(switchBoard)):
-            exporter.write("\t\""+str(switchBoard[printRel][0]) + "\""+ ":\"" + str(switchBoard[printRel][1]) + "\", ")
-            printRel += 1
-        exporter.write("}")
-        exporter.close()
+    exporter = open("rels.txt", "w")
+    exporter.write("{")
+    while(printRel < len(switchBoard)):
+        exporter.write("\t\""+str(switchBoard[printRel][0]) + "\""+ ":\"" + str(switchBoard[printRel][1]) + "\", ")
+        printRel += 1
+    exporter.write("}")
+    exporter.close()
+    print("Rels generated")
 
 # for first level (char conversion)
 # generate unique N length keys for every character value in the chars array
@@ -59,24 +51,14 @@ def charKeysGen(stringKeySize):
             charValues.append([chars[keyIter], key])
             exists.append(key)
             keyIter += 1
-    print("Rels generated")
-    
+
     printKeys = 0 # for conversion; layer one
     #print("charValue = {")
-    if(os.stat("rels.txt").st_size != 0):
-        exporter = open("keys.txt", "w")
-        exporter.write("{")
-        while(printKeys < len(charValues)):
-            exporter.write("\t\""+str(charValues[printKeys][0]) + "\""+ ":\"" + str(charValues[printKeys][1]) + "\", ")
-            printKeys += 1
-        exporter.write("}")
-        exporter.close()
-    else:
-        exporter = open("keys.txt", "a")
-        exporter.write("{")
-        while(printKeys < len(charValues)):
-            exporter.write("\t\""+str(charValues[printKeys][0]) + "\""+ ":\"" + str(charValues[printKeys][1]) + "\", ")
-            printKeys += 1
-        exporter.write("}")
-        exporter.close()
-    print("Keys generates")
+    exporter = open("keys.txt", "w")
+    exporter.write("{")
+    while(printKeys < len(charValues)):
+        exporter.write("\t\""+str(charValues[printKeys][0]) + "\""+ ":\"" + str(charValues[printKeys][1]) + "\", ")
+        printKeys += 1
+    exporter.write("}")
+    exporter.close()
+    print("Keys generated")
