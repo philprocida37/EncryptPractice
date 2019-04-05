39da479f6f5080d6347c1d2cb86d52561b37b2f0
Phil Procida <42648500+philprocida37@users.noreply.github.com>
2019-04-04 21:08:42 -0400

    Small changes to main.py
    Added run time trackers

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 from keyGenerator import charRelsGen, charKeysGen
 from encryption import convertMessage, deconvertMessage, encodeLayerTwo, decodeLayerTwo
-
-choice = int(input("Enter 1 to generate keys & rels\nEnter 2 to encrypt\nEnter 3 to decrypt\n"))
+from datetime import datetime as dt
 def menu(choice):
     if(choice == 1):
         try:
-            r = int(input("\nEnter a key size: "))
+            print("\nProceeding by entering a key size will delete any key and relation values that currently exist.")
+            r = int(input("Enter a key size: "))
+            start = dt.now()
             charRelsGen()
             charKeysGen(r)
+            end = dt.now()-start
+            print('Elapsed time: ', end)
         except:
            print("Not a number.")
     if(choice == 2):
         try:
-            r = str(input("\nEnter a message to encode: "))
+            print("\nMake sure you created your keys and relations first.")
+            r = str(input("Enter a message to encode: "))
+            start = dt.now()
             r = r.replace(" ", "_")
             r = convertMessage(r)
             encodeLayerTwo(r)
+            end = dt.now()-start
+            print('Elapsed time: ', end)
         except:
             print("Error")
     if(choice == 3):
         try:
+            start = dt.now()
             r = deconvertMessage(decodeLayerTwo())
             r = r.replace("_", " ")
             print("Message: "+r)
+            end = dt.now()-start
+            print('Elapsed time: ', end)
         except:
             print("Error")
 try:
-	menu(choice)
+    choice = int(input("Enter 1 to generate keys & rels\nEnter 2 to encrypt\nEnter 3 to decrypt "))
+    acceptables = [1,2,3]
+    while(choice not in acceptables):
+        choice = int(input("Enter 1 to generate keys & rels\nEnter 2 to encrypt\nEnter 3 to decrypt "))
 except:
-	while(choice != 1 and choice != 2 and choice != 3):
-		choice = int(input("Enter 1 to generate keys & rels\nEnter 2 to encrypt\nEnter 3 to decrypt "))
-	menu(choice)
-	
+    print("Not a number.")
+menu(choice)
+    
